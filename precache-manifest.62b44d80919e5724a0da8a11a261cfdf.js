363af54abf3945e1b118dc1c44a4ab6a580c5cd1
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-01-19 05:07:24 +0530

    Updates

---
+++ precache-manifest.62b44d80919e5724a0da8a11a261cfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7ce1fd170f9804b3b2c84fdcb443b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dff4172313a49053c9cf",
+    "url": "/monsters-rolodex/static/css/main.741a8930.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dff4172313a49053c9cf",
+    "url": "/monsters-rolodex/static/js/main.a10f3993.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
